54763ba3e9f976b5521756e2fc5001879b04a306
RizkyEka-Pratama <kuliahrizkyeka@gmail.com>
2026-08-29 11:20:02 +0700

    Delete Project directory

--- Project/Latihan27.java
+++
@@ -1,9 +0,0 @@
-public class Latihan27 {
-
-    public static void main(String[] args){
-        System.out.println("Halo, nama saya adalah Rizky");
-
-        System.out.print("\nBelajar Java itu mudah");
-        //System.out.print("\nSemakin sering mencoba, semakin jago.");
-    }
-}
